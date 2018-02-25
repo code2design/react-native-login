a5dd763c02b9c78d38dcf81782c5c2e63e1b2b08
Suprit <suprit@Suprits-MBP.home>
2018-02-25 14:57:34 -0500

    first commit

---
+++ app/components/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import { StackNavigator } from 'react-navigation';
+
+
+export default class Login extends Component<Props> {
+  render() {
+    return (
+    	<Text>Hello world</Text>
+    );
+  }
+}
+
